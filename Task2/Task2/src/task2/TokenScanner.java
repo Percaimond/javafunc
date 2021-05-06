5c2f8fd7e6a06e3729f20b1c86c0224e0c3e6666
Daniel <danielweissenborn@outlook.de>
2021-05-06 22:42:16 +0200

    little bit of refactoring

--- Task2/Task2/src/task2/TokenScanner.java
+++ Task2/Task2/src/task2/TokenScanner.java
@@ -15,39 +15,23 @@ public class TokenScanner {
     public static List<String> scanner(String input) {
         List<String> results = new ArrayList<>();
 
-        //special case for ; , *
-        //keywords select delete update where from big case
-        //ignore \n \r \t
-        //tokens
-        //null if illegal identifier
-        //a-z A-Z 1-9
         char[] candidates = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w',
                 'x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W',
                 'X','Y','Z'};
         char[] figures = {'1','2','3','4','5','6','7','8','9','0'};
-
         char[] skipCandidates = { '\n', '\r', '\t'};
         char[] sentenceCandidates = {';',',','*'};
         StringBuilder currentToken = new StringBuilder();
         char[] characterArray = new char[input.length()];
-        //char lastChar;
+
         for (int i = 0; i < input.length(); i++) {
             characterArray[i] = input.charAt(i);
         }
-        //for (char c : characterArray) {
-        //System.out.println(c);
-        //}
-        //fehlt edge case zwischen sentenceCandidates und letter und sentenceCandidates
-        //und last iteration
-
 
         for(int k = 1; k < characterArray.length-1; k++){
-            //lastChar = characterArray[k];
+
         }
         for(int j = 0; j < characterArray.length; j++){
-
-
-
             char nextChar = characterArray[j];
             char lastChar;
             if(j==0){
@@ -56,7 +40,6 @@ public class TokenScanner {
             else{
                 lastChar = characterArray[j-1];
             }
-            //char lastChar = characterArray[j-1];
             if((nextChar == ' ')||charEquals(nextChar,skipCandidates)){
                 if(currentToken.length()==0) {
                     continue;
@@ -78,14 +61,9 @@ public class TokenScanner {
                 }
             }else if(((charEquals(nextChar,figures))&&(charEquals(lastChar,candidates)))||
                     ((charEquals(nextChar,candidates))&&(charEquals(lastChar,figures)))) {
-                if(currentToken.length()==0) {
-                    currentToken.append(nextChar);
-                    results.add(String.valueOf(currentToken));
-                }else {
                     results.add(String.valueOf(currentToken));
                     currentToken.setLength(0);
                     currentToken.append(nextChar);
-                }
             }else if(!((charEquals(nextChar,candidates))||((charEquals(nextChar,figures))))){
                 return null;
             }else if(j==characterArray.length-1){
@@ -98,8 +76,7 @@ public class TokenScanner {
         return results;
     }
     public static void main(String[] args){
-        String testtingmesting = "78787878uiuiuiuiui 7uu7uu7uu7uu7u\n7  hjfjh ff ";
+        String testtingmesting = "u78787878uiuiuiuiui 7uu7uu7uu7uu7u\n7  hjfjh ff\n\n\n\n\n\n ";
         System.out.println(scanner(testtingmesting));
-        //System.out.println(lastChar);
     }
 }
